75b2de11010b6f373bf0d3cabfaf167d460274f9
Miguel2003-dell <miguelangelmanzanillacastro@gmail.com>
2024-07-05 17:10:10 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IonicProyect',
+  webDir: 'www'
+};
+
+export default config;
